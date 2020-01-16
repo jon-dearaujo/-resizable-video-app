a99a591ea44c73926fafc0860142a0a34eaa1c63
Jonathan Henrique de Araújo Silva <jonathandearaujosilva@gmail.com>
2020-01-16 14:14:15 -0300

    Refactoring of App.js to use Hooks

--- src/App.js
+++ src/App.js
@@ -1,38 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import ResizableVideo from './resizable-video/ResizableVideo';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {stream: null, error: null}
+// Min 480p - Max 1080p
+const videoConstraints = () => ({
+  audio: false,
+  video: {
+    width: {
+      min: 640,
+      ideal: 1280,
+      max: 1920
+    },
+    height: {
+      min: 480,
+      ideal: 720,
+      max: 1080
+    },
+    facingMode: 'environment'
   }
+});  
+
+export default function App(_) {
+  const [stream, setStream] = useState(null);
+  const [error, setError] = useState(null);
   
-  componentDidMount() {
+  useEffect(() => {
     const {mediaDevices} = window.navigator;
     if (!mediaDevices) {
-      this.setState({error: 'Cannot access webcam. Are you running out of localhost or without https?'});
+      setError('Cannot access webcam. Are you running out of localhost or without https?');
     } else {
-      mediaDevices.getUserMedia(this.videoConstraints())
+      mediaDevices.getUserMedia(videoConstraints())
         .then(s => {
-          this.setState({stream: s});
+          setStream(s);
         })
-        .catch(e => this.setState({error: e.message}));
+        .catch(e => setError(e.message));
     }
-  }
 
-  componentWillUnmount() {
-    if (this.state.stream) {
-      this.state.stream.getVideoTracks().forEach(vt => vt.stop());
+    return () => {
+      if (stream) {
+        stream.getVideoTracks().forEach(vt => vt.stop());
+      }
     }
-  }
+  });
   
-  render = () => {
-    const textContent = this.state.error ? this.state.error : 'Loading...';
-    const videoErrorOrLoadingContent = this.state.stream ?
-     <ResizableVideo srcObject={this.state.stream} containerClassName="App-video"/>
-     : <span className="App-feedbackMessage">{textContent}</span>
-    return (
+  const textContent = error ? error : 'Loading...';
+  const videoErrorOrLoadingContent = stream ?
+   <ResizableVideo srcObject={stream} containerClassName="App-video"/>
+   : <span className="App-feedbackMessage">{textContent}</span>;
+  
+  return (
     <div className="App">
       <ul className="App-side_menu">
         <li>Menu Item 1</li>
@@ -44,24 +60,5 @@ export default class App extends React.Component {
       
       {videoErrorOrLoadingContent}
     </div>
-    );
-  }
-
-  // Min 480p - Max 1080p
-  videoConstraints = () => ({
-    audio: false,
-    video: {
-      width: {
-        min: 640,
-        ideal: 1280,
-        max: 1920
-      },
-      height: {
-        min: 480,
-        ideal: 720,
-        max: 1080
-      },
-      facingMode: 'environment'
-    }
-  });  
+  );
 }
